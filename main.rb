b7f86bc9a53cb7624521cf18ff4eab2c20b49d93
Anton <tohvdeev@gmail.com>
2021-05-07 01:37:00 +0300

    Store v1. Products from files

--- main.rb
+++ main.rb
@@ -1,7 +1,11 @@
 require_relative 'lib/product'
 require_relative 'lib/book'
 require_relative 'lib/film'
+require_relative 'lib/product_collection'
 
-
-leon = Film.new(price: 990, amount: 5)
-puts  "Фильм Леон стоит: #{leon.price} руб."
+current_path = File.dirname(__FILE__)
+collection = ProductCollection.from_dir(current_path + '/data')
+collection.sort!(by: :price, order: :asc)
+collection.to_a.each do |product|
+  puts product
+end
